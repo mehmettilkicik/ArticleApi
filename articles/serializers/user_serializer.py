d6adac2066b3a6662a16baf371311391b3ce0897
mehmettilkicik <pbmemedo@gmail.com>
2025-04-24 18:00:13 +0300

    user serializer finished

---
+++ articles/serializers/user_serializer.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True,validators=[validate_password])
+    password2 = serializers.CharField(write_only=True,required=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password',
+            'password2',
+        ]
+        extra_kwargs = {
+            'username':{'required':True},
+        }
